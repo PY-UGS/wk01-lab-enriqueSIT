39aa8c717890b359e7494a6c8e2e835f4ef0b0ee
ishorty <ishorty112519@gmail.com>
2022-01-11 11:58:23 +0800

    lab1 work

---
+++ Example1.java
@@ -0,0 +1,11 @@
+
+
+
+public class Example1
+{
+    public static void main(String args[])
+    {
+        String yourName = "Enrique";
+        System.out.println(String.format("Hello I am %s!", yourName));
+    }
+}
---
+++ Example4.java
@@ -0,0 +1,14 @@
+
+
+
+public class Example4 
+{
+    public static void main(String args[]) 
+    {
+        // String iValues = "Values of i : ";
+        for (int i = 102; i > 66; i--)
+        {
+            if((i & 1) == 1) System.out.print(i + ", ");
+        }
+    }
+}
